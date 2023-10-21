eb38fd56436ae2d051f858ae71aaff7ab6f48b88
dylan h <baolong281@gmail.com>
2023-10-21 15:28:27 -0500

    fix: cleaned up uploadthing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
                     <Spinner />
                 </div>
             )}
-            <UploadButton
+             <UploadButton
                 endpoint="imageUploader"
                 onClientUploadComplete={(res) => {
                     // Do something with the response
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,13 @@
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
+import { withUt } from "uploadthing/tw";
 
-export default {
-  content: ["./src/**/*.tsx"],
-  theme: {
-    extend: {
-      fontFamily: {},
+export default withUt({
+    content: ["./src/**/*.tsx"],
+    theme: {
+        extend: {
+            fontFamily: {},
+        },
     },
-  },
-  plugins: [],
-} satisfies Config;
+    plugins: [],
+}) satisfies Config;
