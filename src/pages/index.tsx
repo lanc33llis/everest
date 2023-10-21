2274f3442c45f113098bf6690679083c151d3135
dylan h <baolong281@gmail.com>
2023-10-21 12:26:15 -0500

    new: auth
    Signed-off-by: dylan h <baolong281@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,16 @@
 // import { signIn, signOut, useSession } from "next-auth/react";
 import { api } from "~/utils/api";
+import { signIn } from "next-auth/react"
 
 export default function Home() {
-  return <div className="font-sans">Hello world!</div>;
+    return (
+        <>
+        <div>
+            <div className="font-sans">Hello world!</div>
+            <button onClick={() => signIn()}>Sign in</button>
+        </div>
+        </>
+    )
+
+
 }
