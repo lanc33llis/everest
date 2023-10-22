4b1fa184abefc2cbb4a4f2a876b4f68c5fa8af22
Lance Ellis <scdelance@gmail.com>
2023-10-22 10:27:25 -0500

    added script back

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,17 @@
  */
 await import("./src/env.mjs");
 
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
+}
+
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
